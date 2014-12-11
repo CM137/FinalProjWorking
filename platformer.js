99ed98443b2c07ba4ce4687bc4a9512c133adefa
CM137 <ChrisMason137@gmail.com>
2014-12-11 03:08:43 -0500

    Try
    Try

--- platformer.js
+++ platformer.js
@@ -26,7 +26,7 @@ Q.input.touchControls({
                ['right','>' ],
                [],
                [],
-               ['up','^']]
+               ['up','jump']]
 });
 
 // ## Player Sprite
